7812d815b02c7ed1f359d0fad692fe1211b227d4
Marcin Chodorek <marcin.chodorek3@gmail.com>
2020-11-05 19:45:56 +0100

    Add turn info Add template for points Fixed players' placing

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,14 @@
 const playground = document.querySelector('.playground');
 const blocks = document.querySelectorAll('.block');
 const reset = document.querySelector('.reset-btn');
+const message = document.querySelector('.message');
 
 let isActive = true;
 let board_state = ['', '', '', '', '', '', '', '', ''];
+const playersPoints = class {
+    player1 = 0;
+    player2 = 0;
+}
 
 //  !Allowing player to change the icon --in progress--
 // const firstPlayer = document.getElementById('player-one-icon');
@@ -16,20 +21,47 @@ let board_state = ['', '', '', '', '', '', '', '', ''];
 
 let currentPlayer = 'X';
 
+const winMessage = () => `${currentPlayer} Won!`;
+const tieMessage = () => 'It was a Tie!';
+const turnMessage = () => `It's ${currentPlayer}'s Turn!`;
+
+message.innerHTML = turnMessage();
+
 const playerAction = (e) => {
     const clickedBlock = e.target;
     const clickedBlockIndex = parseInt(clickedBlock.getAttribute('id'));
 
-    board_state[clickedBlockIndex] = currentPlayer;
+    if (board_state[clickedBlockIndex]) {
+        return;
+    } else board_state[clickedBlockIndex] = currentPlayer;
     blocks[clickedBlockIndex].innerHTML = currentPlayer;
-
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    message.innerHTML = turnMessage();
 };
+
+
+const handleResult = () => {
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+
+}
 
 const resetPlayground = () => {
     board_state = ['', '', '', '', '', '', '', '', ''];
+    message.innerHTML = "";
     blocks.forEach(block => {
         block.innerHTML = '';
     });
+    message.innerHTML = turnMessage();
 };
 
 reset.addEventListener('click', resetPlayground);
